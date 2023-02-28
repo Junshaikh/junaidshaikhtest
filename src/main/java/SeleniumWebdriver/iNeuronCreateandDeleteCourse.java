188ec63fd5713cb100eb64637fb8bec12f539fd1
Junaid Shaikh <44426749+Junshaikh@users.noreply.github.com>
2023-02-28 08:46:57 +0530

    Added a comment for pull request
    Added a comment for pull request

--- src/main/java/SeleniumWebdriver/iNeuronCreateandDeleteCourse.java
+++ src/main/java/SeleniumWebdriver/iNeuronCreateandDeleteCourse.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+// Added a comment for pull request
 
 public class iNeuronCreateandDeleteCourse {
 
